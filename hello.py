0fe6349546d38ac63a375e29cd6281831609ff83
lee-taekgyu <pickyu2@naver.com>
2020-07-20 20:12:08 +0900

    test

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 #! /usr/bin/env python
 
-print("Hello Lee, your are a experienced people"))
+num1 = 3
+num2 = 5
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 % num2)
